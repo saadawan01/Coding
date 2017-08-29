3439a47e478dd8569edc244ec5b302e1197860c2
Saad Awan <your.msaadawan0@gmail.com>
2017-08-29 18:46:05 +0500

    Initialize repo

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,11 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def hello
+    render html: "¡hello, world!"
+  end
+  def goodbye
+    render html: "Bye Bye!"
+  end
+
+end
